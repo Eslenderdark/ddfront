161aca95b1800f2a7d9f8972b2ad5f236c18e96f
Adrian <eslenderdark@gmail.com>
2026-01-08 18:00:19 +0100

    char

--- src/app/characters/characters.page.ts
+++ src/app/characters/characters.page.ts
@@ -1,36 +1,76 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar,IonGrid,IonRow,IonCol,IonCard,IonCardContent,IonIcon,IonButton} from '@ionic/angular/standalone';
+import {
+  IonContent, IonHeader, IonTitle, IonToolbar,
+  IonGrid, IonRow, IonCol,
+  IonCard, IonCardContent,
+  IonIcon, IonButton,
+  IonModal, IonItem, IonLabel, IonInput,
+  IonSelect, IonSelectOption, IonRange, IonButtons
+} from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { add, addCircleOutline, heart } from 'ionicons/icons';
+import { addCircleOutline } from 'ionicons/icons';
 
-addIcons({
-  add,
-  addCircleOutline,
-  heart
-});
+addIcons({ addCircleOutline });
 
 @Component({
   selector: 'app-characters',
   templateUrl: './characters.page.html',
   styleUrls: ['./characters.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule,IonGrid,IonRow,IonCol,IonCard,IonCardContent,IonIcon,IonButton]
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonContent, IonHeader, IonTitle, IonToolbar,
+    IonGrid, IonRow, IonCol,
+    IonCard, IonCardContent,
+    IonIcon, IonButton,
+    IonModal, IonItem, IonLabel, IonInput,
+    IonSelect, IonSelectOption, IonRange, IonButtons
+  ]
 })
 export class CharactersPage implements OnInit {
-  route: any;
 
-  
+  isCreateModalOpen = false;
+
+  newCharacter = {
+    name: '',
+    race: '',
+    stats: {
+      strength: 5,
+      agility: 5,
+      health: 100
+    }
+  };
+
   constructor() {
-      addIcons({addCircleOutline}); }
+      addIcons({addCircleOutline});}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  movePage() {
+    console.log('Moving to page');
   }
 
-  async movePage() {
-    console.log('Moving to page: ');
-    this.route.navigate(['/start-menu']);
+  openCreateModal() {
+    this.isCreateModalOpen = true;
   }
 
+  closeCreateModal() {
+    this.isCreateModalOpen = false;
+  }
+
+  createCharacter() {
+    if (!this.newCharacter.name || !this.newCharacter.race) {
+      console.log('Fill all fields');
+      return;
+    }
+
+    console.log('Character created:', this.newCharacter);
+
+    // Aquí luego puedes guardarlo en un array o backend
+
+    this.closeCreateModal();
+  }
 }
