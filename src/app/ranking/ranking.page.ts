feb047955009c77c8660519cf909f3d0ff9ef138
Ruben <papagiorgoo@gmail.com>
2026-01-15 16:29:33 +0100

    menu button ranking

--- src/app/ranking/ranking.page.ts
+++ src/app/ranking/ranking.page.ts
@@ -3,18 +3,18 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar,IonList,IonItem,IonLabel } from '@ionic/angular/standalone';
 import { HttpClient } from '@angular/common/http';
-
+import { RouterModule, Router } from '@angular/router';
 
 @Component({
   selector: 'app-ranking',
   templateUrl: './ranking.page.html',
   styleUrls: ['./ranking.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonList,IonItem,IonLabel]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonList,IonItem,IonLabel,RouterModule]
 })
 export class RankingPage implements OnInit {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,private router: Router) { }
 public url_host = 'http://localhost:3000/';
 public ranking: any[] = [];
 
@@ -28,5 +28,8 @@ public ranking: any[] = [];
       this.ranking = response;
       console.log(this.ranking)
     });
+}
+goToMenu() {
+  this.router.navigate(['/start-menu']);
 }
 }
