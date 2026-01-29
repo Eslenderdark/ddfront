ccf82627f336702ca6eedc9de99b57b0bfb19dee
papagiorgo-ded <papagiorgoo@gmail.com>
2026-01-29 12:09:59 +0100

    inventario ALPHA

--- src/app/inventory/inventory.page.ts
+++ src/app/inventory/inventory.page.ts
@@ -1,9 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, signal, WritableSignal } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector:'app-inventory',
@@ -17,56 +16,62 @@ import { HttpClientModule } from '@angular/common/http';
 })
 export class InventoryPage implements OnInit{
 
-  items:any[]=[];
-  characters:any[]=[];
-  userId="1";
+  characters: WritableSignal<any[]> = signal([]);
+  items: WritableSignal<any[]> = signal([]);
+  public url_host = 'http://localhost:3000/';
+  public user_email: string = "";
 
   constructor(private http:HttpClient){}
 
   ngOnInit(){
+    const userStr = localStorage.getItem('user');
+    if (userStr) {
+      const user = JSON.parse(userStr);
+      this.user_email = user.email;
+    }
     this.loadInventory();
-    this.loadCharacters();
   }
 
   loadInventory(){
-    this.http.get<any>(
-      `http://localhost:3000/inventory/${this.userId}`
-    ).subscribe(r=>this.items=r.items);
+    if(!this.user_email) return;
+
+    this.http.get<any>(`${this.url_host}getcharactersbyemail/${encodeURIComponent(this.user_email)}`)
+      .subscribe(res => {
+        console.log('BACK RESPONSE:', res);
+        this.characters.set(res.characters);
+        this.items.set(res.items);
+      });
   }
 
-  loadCharacters(){
-    this.http.get<any>(
-      `http://localhost:3000/characters/user/${this.userId}`
-    ).subscribe(r=>this.characters=r.characters);
+  getSellPrice(r: string){
+    return r=='común'?50:
+           r=='raro'?150:
+           r=='épico'?400:
+           r=='legendario'?1000:20;
   }
 
-  getSellPrice(r:string){
-    return r=='common'?50:
-           r=='rare'?150:
-           r=='epic'?400:
-           r=='legendary'?1000:20;
-  }
-
-  assignItem(i:any,c:number){
-    this.http.post(
-      'http://localhost:3000/inventory/assign',
-      {itemId:i.id,characterId:c}
-    ).subscribe();
+  assignItem(i:any, c:number){
+    this.http.post(`${this.url_host}inventory/assign`, {itemId:i.id, characterId:c})
+      .subscribe();
   }
 
   sellItem(i:any){
-    const p=this.getSellPrice(i.rarity);
-
-    this.http.post(
-      'http://localhost:3000/inventory/sell',
-      {itemId:i.id,price:p}
-    ).subscribe(()=>this.items=this.items.filter(x=>x.id!=i.id));
+    const p=this.getSellPrice(i.rareza);
+    this.http.post(`${this.url_host}inventory/sell`, {itemId:i.id, price:p})
+      .subscribe(()=> this.items.set(this.items().filter(x=>x.id !== i.id)));
   }
 
   publishMarket(i:any){
-    this.http.post(
-      'http://localhost:3000/market/add',
-      {itemId:i.id,price:i.marketPrice}
-    ).subscribe(()=>this.items=this.items.filter(x=>x.id!=i.id));
+    this.http.post(`${this.url_host}market/add`, {itemId:i.id, price:i.marketPrice})
+      .subscribe(()=> this.items.set(this.items().filter(x=>x.id !== i.id)));
   }
+
+  
+
+  // Funciones de ejemplo para mostrar los items (puedes expandirlas)
+  getItemsByCharacter(characterId: number) {
+    return this.items().filter(i => i.character_id === characterId);
+  }
+
 }
+
